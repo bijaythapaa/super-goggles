9211c4bdbe8c7556e1e0c1b2c75aba813b9a0400
dbijaya <dbijaythapa@gmail.com>
2021-01-20 22:34:01 +0545

    [ADD] repostory added for user login request.

--- src/main/java/com/bijay/expensetracker/repositories/UserRepositoryImpl.java
+++ src/main/java/com/bijay/expensetracker/repositories/UserRepositoryImpl.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
@@ -22,6 +23,8 @@ public class UserRepositoryImpl implements UserRepository {
 	private static final String SQL_COUNT_BY_EMAIL = "SELECT COUNT(*) FROM ET_USERS WHERE EMAIL = ?";
 	private static final String SQL_FIND_BY_ID = "SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD "
 			+ "FROM ET_USERS WHERE USER_ID = ?";
+	private static final String SQL_FIND_BY_EMAIL ="SELECT USER_ID, FIRST_NAME, LAST_NAME, EMAIL, PASSWORD "
+			+ "FROM ET_USERS WHERE EMAIL=?";
 
 	@Autowired
 	JdbcTemplate jdbcTemplate;
@@ -47,8 +50,15 @@ public class UserRepositoryImpl implements UserRepository {
 
 	@Override
 	public User findByEmailAndPassword(String email, String password) throws EtAuthException {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			@SuppressWarnings("deprecation")
+			User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, new Object[] {email}, userRowMapper);
+			if(!password.equals(user.getPassword()))
+				throw new EtAuthException("Invalid Email/Password.");
+			return user;
+		} catch (EmptyResultDataAccessException e) {
+			throw new EtAuthException("Invalid email/password.");
+		}
 	}
 
 	@SuppressWarnings("deprecation")
