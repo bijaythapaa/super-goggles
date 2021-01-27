1eb4e59ea73e94193c076871167a8f26e4cf19e0
dbijaya <dbijaythapa@gmail.com>
2021-01-27 23:26:41 +0545

    [ADD] category controller added.

---
+++ src/main/java/com/bijay/expensetracker/resources/CategoryResource.java
@@ -0,0 +1,20 @@
+package com.bijay.expensetracker.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryResource {
+
+    @GetMapping(value = "")
+    public String getAllCategories(HttpServletRequest request) {
+        int userId = (Integer) request.getAttribute("userId");
+        return "Authenticated! userId: '" + userId + "'";
+    }
+
+}
