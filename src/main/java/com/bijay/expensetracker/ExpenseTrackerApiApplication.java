6c4f4d84026da05f8105f6e2dc20489c948e7fc0
dbijaya <dbijaythapa@gmail.com>
2021-01-27 23:24:52 +0545

    [ADD] filter-registration-bean added for filtering the token-valid requests only.

--- src/main/java/com/bijay/expensetracker/ExpenseTrackerApiApplication.java
+++ src/main/java/com/bijay/expensetracker/ExpenseTrackerApiApplication.java
@@ -1,13 +1,27 @@
 package com.bijay.expensetracker;
 
+import com.bijay.expensetracker.filters.AuthFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+	}
+
+	// using this bean we can register our custom filter.
+	@Bean
+	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+		AuthFilter authFilter = new AuthFilter();
+		registrationBean.setFilter(authFilter);
+		registrationBean.addUrlPatterns("/api/categories/*");
+		return registrationBean;
 	}
 
 }
