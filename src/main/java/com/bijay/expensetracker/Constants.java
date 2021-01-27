1c0c6af056c1c2d865d7b3245528636dc9706976
dbijaya <dbijaythapa@gmail.com>
2021-01-27 15:11:37 +0545

    [ADD] constants file added for jwt token generation.

---
+++ src/main/java/com/bijay/expensetracker/Constants.java
@@ -0,0 +1,10 @@
+package com.bijay.expensetracker;
+
+public class Constants {
+	
+	public static final String API_SECTET_KEY = "expensetrackerapi";
+	
+//	in millisecond
+	public static final long TOKEN_VALIDITY = 2 * 60 * 60 *1000;
+
+}
