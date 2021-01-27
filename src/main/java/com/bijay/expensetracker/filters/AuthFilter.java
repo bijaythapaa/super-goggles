37c79373ffbe97ae8da9a33b8127dd2ac5c7e085
dbijaya <dbijaythapa@gmail.com>
2021-01-27 23:41:15 +0545

    [UPDATE] updated.

--- src/main/java/com/bijay/expensetracker/filters/AuthFilter.java
+++ src/main/java/com/bijay/expensetracker/filters/AuthFilter.java
@@ -23,10 +23,10 @@ public class AuthFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        String authHeader = httpServletRequest.getHeader("Authorization");
+        String authHeader = httpRequest.getHeader("Authorization");
         if (authHeader != null) {
             // its convention to split authHeader using String "Bearer"
             String[] authHeaderArr = authHeader.split("Bearer");
@@ -35,18 +35,18 @@ public class AuthFilter extends GenericFilterBean {
                 try {
                     Claims claims = Jwts.parser().setSigningKey(Constants.API_SECTET_KEY).parseClaimsJws(token)
                             .getBody();
-                    httpServletRequest.setAttribute("userId", Integer.parseInt(claims.get("userId").toString()));
+                    httpRequest.setAttribute("userId", Integer.parseInt(claims.get("userId").toString()));
                 } catch (Exception e) {
-                    httpServletResponse.sendError(HttpStatus.FORBIDDEN.value(), "invalid/expired token");
+                    httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "invalid/expired token");
                     return;
                 }
             } else {
-                httpServletResponse.sendError(HttpStatus.FORBIDDEN.value(),
+                httpResponse.sendError(HttpStatus.FORBIDDEN.value(),
                         "Authorization token must be Bearer [token]");
                 return;
             }
         } else {
-            httpServletResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be Provided.");
+            httpResponse.sendError(HttpStatus.FORBIDDEN.value(), "Authorization token must be Provided.");
             return;
         }
         chain.doFilter(request, response);
