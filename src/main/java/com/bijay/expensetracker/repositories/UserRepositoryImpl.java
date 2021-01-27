052561eeb766db01e4aea8cc22052d2d93b42cc1
dbijaya <dbijaythapa@gmail.com>
2021-01-27 15:12:30 +0545

    [UPDATE] for jwt token authentication.

--- src/main/java/com/bijay/expensetracker/repositories/UserRepositoryImpl.java
+++ src/main/java/com/bijay/expensetracker/repositories/UserRepositoryImpl.java
@@ -54,7 +54,8 @@ public class UserRepositoryImpl implements UserRepository {
 	public User findByEmailAndPassword(String email, String password) throws EtAuthException {
 		try {
 			User user = jdbcTemplate.queryForObject(SQL_FIND_BY_EMAIL, new Object[] {email}, userRowMapper);
-			if(!password.equals(user.getPassword()))
+//			if(!password.equals(user.getPassword()))
+			if(!BCrypt.checkpw(password, user.getPassword()))
 				throw new EtAuthException("Invalid Email/Password.");
 			return user;
 		} catch (EmptyResultDataAccessException e) {
