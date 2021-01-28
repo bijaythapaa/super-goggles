9f79ec7f95603338357adf749b04db71e9b69a13
dbijaya <dbijaythapa@gmail.com>
2021-01-28 17:26:31 +0545

    [UPDATE] updated.

--- src/main/java/com/bijay/expensetracker/Constants.java
+++ src/main/java/com/bijay/expensetracker/Constants.java
@@ -5,6 +5,6 @@ public class Constants {
 	public static final String API_SECTET_KEY = "expensetrackerapi";
 	
 //	in millisecond
-	public static final long TOKEN_VALIDITY = 2 * 60 * 60 *1000;
+	public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
 
 }
