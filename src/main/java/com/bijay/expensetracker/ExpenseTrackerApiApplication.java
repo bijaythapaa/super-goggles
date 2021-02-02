87f4d423df79d83995248bd557f43abf8cabb191
dbijaya <dbijaythapa@gmail.com>
2021-02-02 20:37:12 +0545

    [ADD] FilterRegistrationBean added for cors configuration.

--- src/main/java/com/bijay/expensetracker/ExpenseTrackerApiApplication.java
+++ src/main/java/com/bijay/expensetracker/ExpenseTrackerApiApplication.java
@@ -6,22 +6,40 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+    }
 
-	// using this bean we can register our custom filter.
-	@Bean
-	public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
-		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
-		AuthFilter authFilter = new AuthFilter();
-		registrationBean.setFilter(authFilter);
-		registrationBean.addUrlPatterns("/api/categories/*");
-		return registrationBean;
-	}
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+//		configuration.addAllowedOrigin("http://localhost:5500");
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", configuration);
+        registrationBean.setFilter(new CorsFilter(source));
+        registrationBean.setOrder(0);
+        return registrationBean;
+    }
+
+
+    // using this bean we can register our custom filter.
+    @Bean
+    public FilterRegistrationBean<AuthFilter> filterRegistrationBean() {
+        FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
+        AuthFilter authFilter = new AuthFilter();
+        registrationBean.setFilter(authFilter);
+        registrationBean.addUrlPatterns("/api/categories/*");
+        return registrationBean;
+    }
 
 }
